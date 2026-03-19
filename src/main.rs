2c469498de326a3e83a91e2957c96e301572dcb1
faye <irelia833@gmail.com>
2026-03-19 21:16:10 +0000

    feat: Enable chat loop, for continuous conversation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use display_error_chain::DisplayErrorChain;
 use gemini_rust::Gemini;
 use std::env;
+use std::io;
 use std::process::ExitCode;
 mod fs;
 
@@ -28,17 +29,42 @@ async fn do_main() -> Result<(), Box<dyn std::error::Error>> {
     dotenvy::dotenv()?;
     let api_key = env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY environment variable not set");
     let client = Gemini::new(api_key)?;
-
     let system_prompt = fs::load_system_prompt();
 
+    println!("Hi, Velk'oz here, what would you like to talk about today?\n");
+
+    loop {
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
+
+        if user_input == "/quit" {
+            break;
+        }
+
+        send_loading_indicator();
+
+        let res = send_message_and_get_response(&user_input, &system_prompt, &client).await?;
+        println!("{}\n", res);
+    }
+    Ok(())
+}
+
+async fn send_message_and_get_response(
+    message: &str,
+    system_prompt: &str,
+    client: &Gemini,
+) -> Result<String, Box<dyn std::error::Error>> {
     let res = client
         .generate_content()
         .with_system_prompt(system_prompt)
-        .with_user_message("What makes Rust a good choice for systems programming?")
+        .with_user_message(message)
         .execute()
         .await?;
+    Ok(res.text())
+}
 
-    println!("{}", res.text());
-
-    Ok(())
+fn send_loading_indicator() {
+    println!("Thinking...\n")
 }
